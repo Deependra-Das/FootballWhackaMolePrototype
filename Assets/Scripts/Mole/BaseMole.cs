4d28d4e387ad55bebe4228e8771a610db11c360e
Deependra-Das <iamdeep75@gmail.com>
2026-08-25 13:22:42 +0530

    Added BaseMole script with base behaviour

---
+++ Assets/Scripts/Mole/BaseMole.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using UnityEngine;
+
+namespace FootballWhackaMolePrototype.Mole
+{
+    public abstract class BaseMole : MonoBehaviour
+    {
+        [SerializeField] private MoleTypeEnum _moleType;
+
+        [Header("Movement")]
+        [SerializeField] private float _popUpHeight = 3f;
+        [SerializeField] private float _popUpDuration = 0.15f;
+        [SerializeField] private float _popDownDuration = 0.15f;
+
+        protected bool _isVisible;
+        public bool IsVisible => _isVisible;
+
+        private Coroutine _moleRoutine;
+
+        private Vector3 _hiddenPosition;
+        private Vector3 _visiblePosition;
+
+        public abstract int Score { get; }
+
+        protected abstract float VisibleDuration { get; }
+
+        protected virtual void Awake()
+        {
+            _hiddenPosition = transform.localPosition;
+            _visiblePosition = _hiddenPosition + Vector3.up * _popUpHeight;
+        }
+
+        private void Start()
+        {
+            Show();
+        }
+
+        public virtual void Show()
+        {
+            _moleRoutine = StartCoroutine(MoleRoutine());
+        }
+
+        private IEnumerator MoleRoutine()
+        {
+            _isVisible = true;
+            yield return MoveMole(_hiddenPosition, _visiblePosition, _popUpDuration);
+
+            yield return new WaitForSeconds(VisibleDuration);
+
+            yield return MoveMole(_visiblePosition, _hiddenPosition, _popDownDuration);
+            _isVisible = false;
+
+            StopCoroutine(_moleRoutine);
+            Destroy(gameObject);
+        }
+
+        private IEnumerator MoveMole(Vector3 start, Vector3 target, float duration)
+        {
+            float elapsed = 0f;
+            while (elapsed < duration)
+            {
+                elapsed += Time.deltaTime;
+
+                float timer = Mathf.Clamp01(elapsed / duration);
+                timer = Mathf.SmoothStep(0f, 1f, timer);
+                transform.localPosition = Vector3.Lerp(start, target, timer);
+
+                yield return null;
+            }
+
+            transform.localPosition = target;
+        }
+    }
+}
